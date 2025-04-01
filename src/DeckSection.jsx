71aa2d080eca17582417d2ea68523321232b9ad8
João <joao.codelearning@gmail.com>
2025-04-01 02:50:36 -0300

    ajustes

--- src/DeckSection.jsx
+++ src/DeckSection.jsx
@@ -1,6 +1,6 @@
 export function DeckSection({deck, actualCard, setActualCard, removeFromDeck}){
     return (
-        <section className="container-fluid">
+        <section className="container">
             <h5>Total de cartas: <span id="totalAmount">{deck?.length}</span></h5>
             <h5>ATK Total: <span id="totalAmount">{deck?.reduce((total, card)=> total+card.atk || 0, 0)}</span></h5>
             <h5>DEF Total: <span id="totalAmount">{deck?.reduce((total, card)=> total+card.def || 0, 0)}</span></h5>
